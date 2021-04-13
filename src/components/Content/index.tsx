ff73d583426aab4d1a8087372cbd672060e81c3f
Emilio Papini <emilio.papini@SPSSJC.IT>
2021-04-12 22:20:42 -0300

    Finalizado o  Desafio 2

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,34 @@
+import { MovieCard } from "../../components/MovieCard";
+
+import "./content.scss"
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+export function Content({ movies }: ContentProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
+}
